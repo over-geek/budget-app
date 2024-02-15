e6777d93fe01a94ca5c92f5d9f0ee452e53c0ee4
over-geek <kenagy100@gmail.com>
2024-02-15 03:47:59 +0000

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+  get 'expenses/index'
+  get 'expenses/new'
+  # get "/home/index"
+  resources :groups do
+    resources :expenses, only: [:index, :new, :create]
+  end
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  devise_scope :user do
+    authenticated :user do
+      root 'groups#index', as: :authenticated_root_path
+    end
+    unauthenticated do
+      root "home#index", as: :unauthenticated_root_path
+    end
+  end
 end
