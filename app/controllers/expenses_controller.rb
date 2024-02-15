f7b932ddeb3ef2932bb66da6c9a6449ca7e21a5a
over-geek <kenagy100@gmail.com>
2024-02-15 03:04:49 +0000

    Add expense controller

---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,34 @@
+class ExpensesController < ApplicationController
+  before_action :set_group
+
+  def index
+    @expenses = @group.expenses.order(created_at: :desc)
+  end
+
+  def new
+    @expense = Expense.new
+  end
+
+  def create
+    @expense = Expense.new(expense_params)
+    @expense.user = current_user
+    @expense.group_id = @group.id
+    if @expense.save
+      @expense_group = ExpenseGroup.create(group: @group, expense: @expense)
+      redirect_to group_expenses_path(@group), notice: 'Expense was successfully created.'
+    else
+      puts @expense.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
+  def expense_params
+    params.require(:expense).permit(:name, :amount)
+  end
+end
